7ac08aadd183b1073ce642ecf0a7e071f3d28b43
EricHanJf <jf08942378268@gmail.com>
2024-05-02 12:41:01 +0100

    dashboard link can use

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,9 +5,9 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href=" 'dashboard') }}">
+                    <a href="{{ url('/dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a >
+                    </a>
                 </div>
 
                 <!-- Navigation Links -->
@@ -143,6 +143,5 @@
 
             </div>
         </div>
-        @endauth
     </div>
 </nav>
